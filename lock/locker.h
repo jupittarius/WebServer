2ad61eec028e142fb63f0ded3df6dcff8c6df84c
jupittarius <presdell@outlook.com>
2021-11-14 15:49:20 +0800

    sem_init() function notion

--- lock/locker.h
+++ lock/locker.h
@@ -1,8 +1,42 @@
 #ifndef LOCKER_h
 #define LOCKER_H
+
 #include <exception>
 #include <pthread.h>
 #include <semaphore.h>
+
+/************************************************************************/
+/*                     function notion                                  */
+/*                                                                      */
+/************************************************************************/
+/*
+int sem_init(sem_t* sem, int pshared, unsigned int value);
+
+sem_init() initializes the unnamed semaphore at the address pointed to by sem.
+The 'value' argument specifies the initial value for the semaphore.
+
+The 'pshared' argument indicates whether this semaphore is to be shared 
+    between the threads of a process, or between processes.
+If pshared has the value 0, then the semaphore is shared between
+       the threads of a process, and should be located at some address
+       that is visible to all threads (e.g., a global variable, or a
+       variable allocated dynamically on the heap).
+If pshared is nonzero, then the semaphore is shared between
+       processes, and should be located in a region of shared memory
+       (see shm_open(3), mmap(2), and shmget(2)).  (Since a child
+       created by fork(2) inherits its parent's memory mappings, it can
+       also access the semaphore.)  Any process that can access the
+       shared memory region can operate on the semaphore using
+       sem_post(3), sem_wait(3), and so on.
+
+Initializing a semaphore that has already been initialized
+       results in undefined behavior.
+
+sem_init() returns 0 on success; on error, -1 is returned, and
+       errno is set to indicate the error.
+ */
+
+
 
 class sem{
     public:
